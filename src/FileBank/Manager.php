5a09978a82cda2c958ed23f3251833508e26aed8
Johan <joacub@gmail.com>
2014-04-28 21:49:09 +0200

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- src/FileBank/Manager.php
+++ src/FileBank/Manager.php
@@ -766,7 +766,6 @@ class Manager
             $file->setDownloadUrl($this->params['s3_base_url'] . $this->params['filebank_folder_aws_s3'] . $file->getSavePath());
             
         } else {
-            exit;
             $urlHelper = $this->sl->get('viewrenderer')
                 ->getEngine()
                 ->plugin('url');
