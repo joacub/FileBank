99cdd5bbcfecd7611aad17d2faae915ee64c1764
Johan Rodriguez Ramos <joacub@gmail.com>
2015-02-26 00:39:32 +0100

    exclude cache headers

--- src/FileBank/Manager.php
+++ src/FileBank/Manager.php
@@ -1708,12 +1708,6 @@ class Manager
 
     public function createFileVersion(File $file)
     {
-        header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
-        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-        header("Cache-Control: no-store, no-cache, must-revalidate");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-
         $version = $file->getVersion();
         if (! $version || file_exists($file->getAbsolutePath())) {
             return;
